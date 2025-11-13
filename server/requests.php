1b6205d0e2067863574db350c0d8262d4287d296
SATYAMP023 <personalphovido@gmail.com>
2025-11-13 10:25:43 +0530

    Add files via upload

---
+++ server/requests.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+include("../common/db.php");
+if(isset($_POST["signup"])){
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $c_password = $_POST["c_password"];
+
+    if ($password === $c_password) {
+        $user = $conn->prepare("Insert into `users`
+        (`id`,`username`,`email`,`password`)
+        values(NULL,'$username','$email','$password');
+        ");
+
+        $result = $user->execute();
+        $user->insert_id;
+        if($result){
+            echo("New User Registered");
+            $_SESSION['user'] = ['username' => $username,'email'=> $email,'user_id'=>$user->insert_id];
+            header("location: /first_project");
+        }else{
+            echo("NOT REGISTERED TRY AGAIN");
+        }
+    }
+    else {
+        echo("Confirm Password is different please try again");
+    }
+}
+else if(isset($_POST["login"])){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $username = "";
+    $user_id = 0;
+    $query = "select * from users where email = '$email' and password = '$password'";
+    $result = $conn->query($query);
+    if($result->num_rows == 1){
+        foreach($result as $row){
+            $username = $row["username"];
+            $user_id = $row["id"];
+        }
+        $_SESSION['user'] = ['username' => $username,'email'=> $email,'user_id'=>$user_id];
+        header("location: /first_project");
+    }else{
+        echo("NOT REGISTERED TRY AGAIN");
+    }
+}
+else if(isset($_GET["logout"])){
+    session_unset();
+    header("location: /first_project");
+}
+else if(isset($_POST["ask"])){
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $category_id = $_POST["category"];
+    $user_id = $_SESSION["user"]["user_id"];
+
+        $question = $conn->prepare("Insert into `questions`
+        (`id`,`title`,`description`,`category_id`,`user_id`)
+        values(NULL,'$title','$description','$category_id','$user_id');
+        ");
+
+        $result = $question->execute();
+        if($result){
+            echo("Your Question Posted");
+            header("location: /first_project");
+        }else{
+            echo("Question Not Posted");
+        }
+
+}
+
+else if(isset($_POST["answer"])){
+    $answer = $_POST["answer"];
+    $question_id = $_POST["question_id"];
+    $user_id = $_SESSION["user"]["user_id"];
+    echo ($answer);
+    echo ($question_id);
+    echo ($user_id);
+
+        $query = $conn->prepare("Insert into `answers`
+        (`id`,`answer`,`question_id`,`user_id`)
+        values(NULL,'$answer','$question_id','$user_id');
+        ");
+
+        $result = $query->execute();
+        if($result){
+            echo("Your Answer Submitted");
+            header("location: /first_project?q-id=$question_id");
+        }else{
+            echo("Answer not submitted successfully");
+        }
+
+}
+
+else if(isset($_GET["delete"])){
+    echo $qid = $_GET["delete"];
+    $query = $conn->prepare("delete from questions where id = $qid");
+    $result = $query->execute();
+        if($result){
+            echo("Question Deleted");
+            header("location: /first_project");
+        }else{
+            echo("Question not Deleted error occured");
+        }
+
+    }
+
+else {
+    //
+}
+
+?>
